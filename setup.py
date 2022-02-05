5b7c4497da5df99fca3b674924358af0e66f8a5a
talwat <talrac1020@gmail.com>
2022-02-05 19:05:13 +0100

    First attempt at getting the package on Pypi.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='py-load-lib',
+    description="A super simple, easy to use module for python.",
+    version='0.3',
+    license='MIT',
+    author="Talwat",
+    author_email='talwat321@gmail.com',
+    py_modules=["py_load"],
+    python_requires='>=3.6, <4',
+    keywords='loading, bar, loadingbar, loading bar',
+    url='https://github.com/talwat/py-load',
+)
