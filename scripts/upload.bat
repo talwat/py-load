13bb8ce04769c92b6b524fb0794295babce8c03b
talwat <talrac1020@gmail.com>
2022-02-06 15:01:57 +0100

    Tweaked Scripts

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,5 +1,5 @@
 rm -r dist
 rm -r *.egg-info
-python setup.py sdist || py setup.py sdist
+py setup.py sdist
 pip install twine
 twine upload dist/*
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,6 +2,6 @@
 
 rm -r dist
 rm -r ./*.egg-info
-python setup.py sdist || py setup.py sdist
-pip install twine
+python3 setup.py sdist
+python3 -m pip install twine
 twine upload dist/*
